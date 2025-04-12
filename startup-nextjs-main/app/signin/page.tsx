878f63af38ac250ed08bf0c4551bbadd40c9bcab
anna karolyne <129319846+AnnaKArolynee@users.noreply.github.com>
2025-04-11 22:02:41 -0300

    page.tsx

--- startup-nextjs-main/app/signin/page.tsx
+++ startup-nextjs-main/app/signin/page.tsx
@@ -63,7 +63,7 @@ const SigninPage = () => {
                 <div className="mb-8 flex items-center justify-center">
                   <span className="hidden h-[1px] w-full max-w-[70px] bg-body-color/50 sm:block"></span>
                   <p className="w-full px-5 text-center text-base font-medium text-body-color">
-                    Ou entre com a sua matrícula
+                    Ou, seu n° matrícula 
                   </p>
                   <span className="hidden h-[1px] w-full max-w-[70px] bg-body-color/50 sm:block"></span>
                 </div>
