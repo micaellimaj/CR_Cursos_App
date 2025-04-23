04af07ff9dc762552cbe0262d45aab1c0ecec79d
anna karolyne <129319846+AnnaKArolynee@users.noreply.github.com>
2025-04-23 12:11:38 -0300

    Update CustomDrawerContent.tsx

--- AppCR/MeuApp/navigation/CustomDrawerContent.tsx
+++ AppCR/MeuApp/navigation/CustomDrawerContent.tsx
@@ -106,6 +106,20 @@ export default function CustomDrawerContent(props: DrawerContentComponentProps) 
         <Text style={[styles.sectionTitle, { color: theme === 'light' ? '#2d3748' : '#e2e8f0' }]}>
           CONFIGURAÇÕES
         </Text>
+
+        <DrawerItem
+    label="Configurações"
+    icon={({ size }) => (
+      <Ionicons
+        name="settings-sharp"
+        size={size}
+        color={theme === 'light' ? '#4a90e2' : '#63b3ed'}
+      />
+    )}
+    onPress={() => props.navigation.navigate('Settings')}
+    style={styles.drawerItem}
+    labelStyle={[styles.drawerLabel, { color: theme === 'light' ? '#2d3748' : '#e2e8f0' }]}
+  />
         
         <DrawerItem
           label="Alternar Tema"
