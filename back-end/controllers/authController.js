9737ce821de3a06895cd6ad2977e7b0fdba843c7
mattheusfelicianoo <matheusfeliciano@protonmail.com>
2025-04-24 23:40:53 -0300

    Revert "Merge branch 'Back-End' into Development"
    This reverts commit 2b8ca812031eb9795c15036f44c6deb8285e3678, reversing
    changes made to eec7cfe59fd79c4e328eb134aca1a9e5b7030983.

--- back-end/controllers/authController.js
+++
@@ -1,7 +0,0 @@
-// lógica de login, 
-
-// Crie rotas específicas de login e registro para alunos e professores, por exemplo:
-
-// * /auth/aluno/login, /auth/professor/login
-
-// ** Ou se quiser simplificar: /login e no body você manda o tipo de usuário
--- back-end/middlewares/authMiddleware.js
+++
@@ -1,2 +0,0 @@
-//middleware para proteger rotas
-// Rotas públicas que não requerem autenticação (login, register, index )
