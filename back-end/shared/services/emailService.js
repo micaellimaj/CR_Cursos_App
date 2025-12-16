73bb1f0bc3bb7836ddbe11d6ed30386819d4164e
Micael Lima <131397330+micaellimaj@users.noreply.github.com>
2025-12-16 09:54:24 -0300

    config. smtp google

--- back-end/shared/services/emailService.js
+++ back-end/shared/services/emailService.js
@@ -2,15 +2,17 @@ const nodemailer = require("nodemailer");
 const hbs = require("nodemailer-express-handlebars").default;
 const path = require("path");
 
+
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: Number(process.env.EMAIL_PORT) || 587,
-  secure: Number(process.env.EMAIL_PORT) === 465,
+  port: Number(process.env.EMAIL_PORT),
+  secure: false, // importante para porta 587
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   }
 });
+
 
 transporter.use(
   "compile",
